2d71dd25101d8e1823061d7eaf9805d157a4d411
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-03-13 23:37:35 +0800

    2

---
+++ hello_world/hello_world/hello_world.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	cout << "Hello, World!" << endl;
+	vector<int> v{ 1, 2, 3, 4, 5 };
+	for(auto it=v.begin(); it!=v.end(); ++it)
+	{
+		cout << *it << " ";
+	}
+	cout << endl << endl;
+	return 0;
+}
